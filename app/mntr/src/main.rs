1781b133ebf104e1abcb19b0f60ea83e0b87a2de
Yarot Kell <mechild02@gmail.com>
2022-01-15 14:35:48 -0500

    [APP] added Julia init and exit hook to app.

--- app/mntr/src/main.rs
+++ app/mntr/src/main.rs
@@ -1,5 +1,16 @@
-
+extern "C" {
+   fn jl_init();
+   fn jl_atexit_hook(i: i32);
+}
 
 fn main() {
+   unsafe {
+      jl_init();
+   }
+
    println!("Hello, world!");
+
+   unsafe {
+      jl_atexit_hook(0);
+   }
 }
