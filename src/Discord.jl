cf8e324d2757f21804303c3d9c83d0b75f93a4e1
Yarot Kell <mechild02@gmail.com>
2022-01-19 06:47:07 -0500

    [DISCORD] moving around client functions.

--- src/Discord.jl
+++ src/Discord.jl
@@ -5,6 +5,33 @@ include("Discord/Client.jl")
 include("Discord/Gateway.jl")
 include("Discord/REST.jl")
 
+
+mutable struct Shard
+   id::UInt64
+end
+
+
+"""mutable struct Client
+
+Represents a Discord client.
+Sends and receives data over a WebSocket connection.
+"""
+mutable struct Client
+   config::Settings
+   shards::Array{Shard}
+end
+
+function Client(settings::Settings)
+   client = Client(settings, [Shard(0)])
+   for i = 0:client.shard_count
+      shard = Shard(i)
+      push!(client.shards, shard)
+   end
+
+   return client
+end
+
+
 export
    Client,
    Shard,
